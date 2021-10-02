3eeb12f92c6634720a54d57bc0044ecbe4fd81a5
Bolaji <layenibolaji5@gmail.com>
2021-10-01 18:26:32 -0700

    stats preview card done

--- src/pages/StatsPreviewCard.js
+++ src/pages/StatsPreviewCard.js
@@ -1,12 +1,12 @@
 const StatsPreviewCard = () => {
   return (
-    <div style={{background: "hsl(233, 47%, 7%)", height: "100vh"}}>
+    <div style={{background: "hsl(233, 47%, 7%)", height: "100vh", zIndex: "-2", }}>
     <div className="container" >
       <div className="row space">
         <div className="col-12"></div>
       </div>
       <div className="row container-fluid ">
-        <div className="col-6 card-text ">
+        <div className="col-md-6 card-text ">
           <h1 className="big-card-text">Get <span className='insights-text'>insights</span> that help your business grow.</h1>
           <p className="sm-card-text">Discover the benefits of data analytics and make better decision regarding revenue, customer 
             experience, and overall efficiency.</p>
@@ -16,7 +16,7 @@ const StatsPreviewCard = () => {
           <span className="fig"><h4 >12M+</h4> <p className="sm-text">QUERIES</p></span>
           </div>
         </div>
-        <div className="col-6 card-image">
+        <div className="col-md-6 card-image">
           <img src="https://raw.githubusercontent.com/saadshaikh1992/stats-preview-card-component/main/image-header-desktop.jpg" alt="main-page-img" class="img-fluid" />
         </div>
         </div>
