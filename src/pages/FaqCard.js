99eb1a3404b85e4a70319feead52a7340cfda534
Bolaji <layenibolaji5@gmail.com>
2021-10-06 17:01:21 -0700

    halfway done with faq car

--- src/pages/FaqCard.js
+++ src/pages/FaqCard.js
@@ -2,17 +2,26 @@ const FaqCard = () => {
   return (
     <div style={{background: "hsl(273, 75%, 66%)", height: "100vh"}}>
       <div className="container">
-        <div className="row faq-box">
-          <div className="col-md-6 container">
+        <div className="row space">
+        <div className="col-12"></div>
+        </div>
+        <div className="row faq-box" >
+            <div className="col-md-6 bg-pattern" >
+            <img src="../../images/FaqC/bgPattern.svg" alt="bg-pattern-image" className=" bg-pattern-image"/>
             <img
-              src="../../images/FaqC/bgPattern.svg"
-              alt="bg-pattern-image"
+              src="../../images/FaqC/womanOnline.svg"
+              alt="woman-online-img"
               class="img-fluid"
-              className="bg-pattern-image"
+              className="woman-online-image"
             />
             </div>
-          <div className="col-md-6">
-              
+          <div className="col-md-6 faq-text">
+            <h1>FAQ</h1>
+            <p>How many team members can I invite?</p>
+            <p>How many team members can I invite?</p>
+            <p>How do I reset my password?</p>
+            <p>Can I cancel my subscription?</p>
+            <p>Do you provide additional support?</p>
           </div>
         </div>
       </div>
